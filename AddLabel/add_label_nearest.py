bbaa2597af61cc73469af2c866ec3c54ea97617b
Huanle Zhang <zhanghuanle1342@gmail.com>
2020-04-29 22:36:38 -0700

    backup

--- AddLabel/add_label_nearest.py
+++ AddLabel/add_label_nearest.py
@@ -17,12 +17,12 @@ import datetime
 scannet_dir = "/home/dtc/Backup/Data/ScanNet"
 
 # Random, Grid, Hierarchy
-data_type = "Random"
+data_type = "Hierarchy"
 
 
 specify_id = []  # if want to valid specific ids
 
-k_KNN = 2  # number of nearest labels
+k_KNN = 1  # number of nearest labels
 
 # --- end of configuration ---
 
